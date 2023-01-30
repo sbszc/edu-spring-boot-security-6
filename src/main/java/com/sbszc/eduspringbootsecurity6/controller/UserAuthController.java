4d27a7c950ec48c8a6aca759f9cf460c7a9ec770
Sebastián <sebastian.zcc@gmail.com>
2023-01-30 08:55:06 -0500

    medium project updates

--- src/main/java/com/sbszc/eduspringbootsecurity6/controller/UserAuthController.java
+++ src/main/java/com/sbszc/eduspringbootsecurity6/controller/UserAuthController.java
@@ -1,5 +1,6 @@
 package com.sbszc.eduspringbootsecurity6.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,16 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserAuthController {
 
     @PostMapping("write")
+//    @PreAuthorize("hasAnyAuthority('write')")
     public String write() {
         return "write";
     }
 
     @GetMapping("read")
+//    @PreAuthorize("hasAnyAuthority('read')")
     public String read() {
         return "read";
     }
 
     @GetMapping("ping")
+//    @PreAuthorize("permitAll()")
     public String ping() {
         return "ping";
     }
--- src/main/java/com/sbszc/eduspringbootsecurity6/security/SecurityConfig.java
+++ src/main/java/com/sbszc/eduspringbootsecurity6/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+//@EnableMethodSecurity
 public class SecurityConfig /*extends WebSecurityConfigurerAdapter CHANGES ON SECURITY 6*/ {
 
     private final UserDetailsService userDetailsService;
@@ -65,7 +67,6 @@ public class SecurityConfig /*extends WebSecurityConfigurerAdapter CHANGES ON SE
 //                .requestMatchers(HttpMethod.POST, "/write").hasAnyRole("ADMIN")
 //                .requestMatchers(HttpMethod.GET, "/read").hasAnyRole("ADMIN", "READ_ONLY")
 //                .requestMatchers(HttpMethod.GET, "/ping").permitAll()
-                .and()
-                .build();
+                .and().build();
     }
 }
